9181beca0e582f72947d9e6d31fca702de4d9139
rafidoth <rafiulhasan803@gmail.com>
2025-04-14 23:58:41 +0600

    maps

--- 7. Maps/maps.go
+++ 7. Maps/maps.go
@@ -1,11 +1,17 @@
 package maps
 
+import "errors"
 
 
+var ErrNotFound = errors.New("could not find the word you're looking for")
 
 type Dictionary map[string]string
 
 
-func (d Dictionary) Search(word string) string{
-	return d[word]
+func (d Dictionary) Search(word string) (string, error){
+	result , ok := d[word]
+	if !ok {
+		return "", ErrNotFound
+	}
+	return result, nil
 }
